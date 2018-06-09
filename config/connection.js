52a9764f74f00d1f86d7612f8906b20d4f55c6bc
Michael Myers <michaelmyers@Michaels-MacBook-Pro.local>
2018-06-09 10:34:39 -0400

    setup

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// Set up MySQL connection.
+var mysql = require("mysql2");
+require("dotenv").config();
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root", 
+    database: "workout2"
+  });
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
